59c73a0858395ef8471bee790c0bca5637a25367
leilaSaeed2014 <leilasaeed2013@gmail.com>
2015-04-09 10:24:31 +0200

    save

--- app/src/main/java/com/FCI/SWE/SocialNetwork/MainActivity.java
+++ app/src/main/java/com/FCI/SWE/SocialNetwork/MainActivity.java
@@ -1,10 +1,8 @@
 package com.FCI.SWE.SocialNetwork;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
